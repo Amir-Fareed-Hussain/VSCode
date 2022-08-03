a8f3474838679e68b8fb7d10e8a22d90b7cbacbc
Amir <amirfareedhussain@gmail.com>
2022-08-03 11:04:44 -0700

    vscode clon start

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Get refrence for wesite
+const Portfolio = document.getElementById('Portfolio')
+
+// Set Events for website
+Portfolio.addEventListener('click',() => {
+    alert('This Website is not responsive.')
+});
